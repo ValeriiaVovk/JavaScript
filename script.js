7d82f0a376f2d32277ab8e985d5b1a0950e529d0
Valeriia Vovk <vovkvaleriya15@gmail.com>
2023-02-24 13:12:40 +0200

    HW from lesson 22

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let firstNumber = prompt('Введіть перше число', 0);
-let secondNumber = prompt('Введіть друге число', 0);
+let firstNumber = prompt('Введіть перше число');
+let secondNumber = prompt('Введіть друге число');
 
 let addition = Number(firstNumber) + Number(secondNumber);
 let subtraction = Number(firstNumber) - Number(secondNumber);
 let multiplication = Number(firstNumber) * Number(secondNumber);
 let division = Number(firstNumber) / Number(secondNumber);
 
-alert(`Сума чисел: ${addition}`); 
-alert(`Різниця чисел: ${subtraction}`); 
-alert(`Добуток чисел: ${multiplication}`);
-alert(`Частка чисел: ${division}`); 
+if (firstNumber === "" || secondNumber === "") {
+  alert(`Помилка в обчисленнях! Введіть числа!`);
+} else {
+  alert(`Сума чисел: ${addition}`);
+
+  if (firstNumber < secondNumber) {
+    let confirmed = confirm(`Ви впевнені, що хочете продовжити операцію?`);
+    if (confirmed) {
+      alert(`Різниця чисел: ${subtraction}`);
+    } else {
+    }
+  } else if (firstNumber >= secondNumber) {
+    alert(`Різниця чисел: ${subtraction}`);
+  }
+
+  alert(`Добуток чисел: ${multiplication}`);
+
+  if (secondNumber == 0) {
+    alert(`Ділити на нуль не можна!`);
+  } else {
+    alert(`Частка чисел: ${division}`);
+  }
+}
